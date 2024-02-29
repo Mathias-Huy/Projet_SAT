72b94b329d552f10089beed86de595fee48753e1
Mathias_Huyghe <mathuyhuy@gmail.com>
2024-02-29 09:22:21 +0100

    Test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import utils
 
-f=open("Gaz.txt")
+sigma = 1 / 600  # m^-1
+f = open("Gaz.txt")
+bdd = []
 
-for line
+for line in f:
+    if line[0] == "#":
+        pass
+    else:
+        l = line.split()
+        name, k0, k1, k2, density = l[0], l[1], l[2], l[3], l[4]
+        index = utils.refrac(k0, k1, k2)
+        bdd.append(utils.Gas(name, density, index))
+f.close()
--- utils.py
+++ utils.py
@@ -7,25 +7,20 @@ from Gas import Gas
 R = 8, 31446261815324  # J/K/mol la constante des gazs parfaits
 masse_molaire_air = 29e-3  # kg/mol
 masse_vol_air = 1.204  # kg/m^3
-sigma = 1 / 600  #
 
 
 class Gas:
-    """Nous allons travailler pour realiser des profils d'indice de reflexion donc il faut etablir les gazs de notre
-    panache et sa densité sur chaque pas delta_r"""
 
-    def __init__(self, name, density, index, compr):
+    def __init__(self, name, density, index):
         self.name = name
         self.density_ctrl = float(density) if density != "None" else None
         self.index_ctrl = float(index) if index != "None" else None
         self.env_ctrl = (self.index_ctrl, self.density_ctrl)
-        self.compr = float(compr)
         self.desity = None
         self.masse_molaire = None
 
     def ciddor(self):
         return (self.desity / self.density_ctrl) * ((self.index_ctrl ** 2 - 1) / (self.index_ctrl ** 2 + 2))
-
 
 
 def density(pression, temperature, masse_molaire, compres=1):
