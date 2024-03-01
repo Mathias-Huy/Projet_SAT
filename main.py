d3b627cface56789f0cb36ed8f30c1b4d9cb9647
Mathias_Huyghe <mathuyhuy@gmail.com>
2024-03-01 18:31:36 +0100

    Test

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import utils
-from ambiance import Atmosphere
 
 sigma = 1 / 600  # m^-1
 f = open("Gaz.txt")
-bdd = []
+bdd = {}
 
 for line in f:
     if line[0] == "#":
         pass
     else:
+        line.replace(",", ".")
         l = line.split()
-        name, k0, k1, k2, density = l[0], l[1], l[2], l[3], l[4]
-        index = utils.refrac(k0, k1, k2)
-        bdd.append(utils.Gas(name, density, index))
+        name, k0, k1, k2, density = l[0], float(l[1]), float(l[2]), float(l[3]), float(l[4])
+        index = utils.refrac(k0, k1, k2, sigma)
+        bdd[name] = utils.Gas(name, density, index)
 f.close()
 
-#test
+comp_air = [(bdd["O2"], 0.5), (bdd["N2"], 0.5)]
+air_std = utils.Atmo(0, 30, comp_air)
+
+tranche = air_std.decoupe(300)
+
+print(bdd)
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
-import math
+import math as m
 import numpy as np
-from Gas import Gas
 
 """On initialise les gaz pour une longueur d'onde de 60m"""
 
@@ -11,23 +10,55 @@ masse_vol_air = 1.204  # kg/m^3
 
 class Gas:
 
-    def __init__(self, name, density, index):
+    def __init__(self, name, dens, index):
         self.name = name
-        self.density_ctrl = float(density) if density != "None" else None
+        self.density_ctrl = float(dens) if density != "None" else None
         self.index_ctrl = float(index) if index != "None" else None
         self.env_ctrl = (self.index_ctrl, self.density_ctrl)
-        self.desity = None
-        self.masse_molaire = None
 
     def ciddor(self):
         return (self.desity / self.density_ctrl) * ((self.index_ctrl ** 2 - 1) / (self.index_ctrl ** 2 + 2))
 
 
-def density(pression, temperature, masse_molaire, compres=1):
-    masse_vol = (masse_molaire * pression) / (R * temperature * compres)
+class Atmo:
+
+    def __init__(self, altitude_deb, altitude_fin, liste):
+        """altitude en km"""
+        self.debut = altitude_deb
+        self.fin = altitude_fin
+        self.comp = liste
+
+    def decoupe(self, tranche):
+        liste = []
+        pas = (self.fin - self.debut) / tranche
+        for x in range(tranche):
+            liste.append((self.debut + x * pas, pression(self.debut + x * pas), temperature(self.debut + x * pas)))
+
+
+def density(press, temp, masse_molaire, compres=1):
+    masse_vol = (masse_molaire * press) / (R * temp * compres)
 
     return masse_vol / masse_vol_air
 
 
 def refrac(k0, k1, k2, sigma):
     return 1 + 10e-8 * (k0 + (k1 / (k2 - sigma ** 2)))
+
+
+def temperature(altitude):
+    """altitude en km et retourne la temperature en kelvin"""
+    if altitude < 13:
+        return 294.9838 - 5.2159 * altitude - 0.07109 * altitude ** 2
+    elif 13 <= altitude < 17:
+        return 215.5
+    elif 17 <= altitude < 47:
+        return 215.5 * m.exp((altitude - 17) * 0.008128)
+
+
+def pression(altitude):
+    """Pression en hPa avec l'altitude en km"""
+
+    if altitude <= 10:
+        return 1012.8186 - 111.5569 * altitude + 3.8646 * altitude ** 2
+    elif 10 < altitude <= 72:
+        return pression(10) * m.exp(-0.417 * (altitude - 10))
