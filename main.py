0eaa4c4f7891697e6e69bb420a463b845d0775be
Pierre MALOD <119065831+PierreMalod@users.noreply.github.com>
2024-03-13 08:48:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,19 @@ f.close()
 comp_air_std = [(bdd["O2"], 0.2), (bdd["N2"], 0.8)]
 comp_air_co = [(bdd["O2"], 0.189), (bdd["N2"], 0.711), (bdd["CO"], 0.1)]
 
-# atmosphere_std = utils.Atmo(0, 30, comp_air_std)
-# atmosphere_std.decoupe_altitude(30)
-# atmosphere_std.profil_indice()
-# temperatures = atmosphere_std.temperatures
-# pressions = atmosphere_std.pressions
-# indices = atmosphere_std.indices
-# altitudes = atmosphere_std.altitudes
+
+# Pour l'atmosphère standard : 
+
+atmosphere_std = utils.Atmo(0, 30, comp_air_std)
+atmosphere_std.decoupe_altitude(30)
+atmosphere_std.profil_indice()
+
+pressions = atmosphere_std.pressions
+temperatures = atmosphere_std.temperatures
+indices = atmosphere_std.indices
+altitudes = atmosphere_std.altitudes
+
+# Pour l'atmosphère avec couche de monoxyde de carbone CO :
 
 atmosphere_std_1 = utils.Atmo(0, 10, comp_air_std)
 atmosphere_co = utils.Atmo(10, 20, comp_air_co)
@@ -36,31 +42,34 @@ atmosphere_std_1.profil_indice()
 atmosphere_co.profil_indice()
 atmosphere_std_2.profil_indice()
 
-pressions = atmosphere_std_1.pressions + atmosphere_co.pressions + atmosphere_std_2.pressions
-temperatures = atmosphere_std_1.temperatures + atmosphere_co.temperatures + atmosphere_std_2.temperatures
-indices = atmosphere_std_1.indices + atmosphere_co.indices + atmosphere_std_2.indices
-altitudes = atmosphere_std_1.altitudes + atmosphere_co.altitudes + atmosphere_std_2.altitudes
+pressions_co = atmosphere_std_1.pressions + atmosphere_co.pressions + atmosphere_std_2.pressions
+temperatures_co = atmosphere_std_1.temperatures + atmosphere_co.temperatures + atmosphere_std_2.temperatures
+indices_co = atmosphere_std_1.indices + atmosphere_co.indices + atmosphere_std_2.indices
+altitudes_co = atmosphere_std_1.altitudes + atmosphere_co.altitudes + atmosphere_std_2.altitudes
 
 modele_itu = utils.ITU(pressions, temperatures)
 
 plt.figure("Temperature")
 plt.plot(temperatures, altitudes)
+plt.plot(temperatures_co, altitudes_co)
 plt.title("Temperature en fonction de l'altitude")
 plt.xlabel("Temperature en K")
 plt.ylabel("Altitude en km")
 
 plt.figure("Pression")
 plt.plot([i / 100 for i in pressions], altitudes)
+plt.plot([i / 100 for i in pressions_co], altitudes_co)
 plt.title("Pression en fonction de l'altitude")
 plt.xscale("log")
 plt.xlabel("Pression en hPa")
 plt.ylabel("Altitude en km")
 
 plt.figure("Réfractivité")
-plt.plot(indices, altitudes)
-plt.plot(modele_itu, altitudes, ls=':')
+plt.plot(indices, altitudes, label = "Indices")
+plt.plot(modele_itu, altitudes, ls=':', label = "modèle ITU")
+plt.plot(indices_co, altitudes_co, label = "Indices CO")
 plt.title("Réfractivité en fonction de l'altitude")
 plt.xlabel("Réfractivité en N unit")
 plt.ylabel("Altitude en km")
-
+plt.legend()
 plt.show()
